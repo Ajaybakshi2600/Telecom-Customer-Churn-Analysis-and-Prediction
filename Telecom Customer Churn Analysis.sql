2e96a761f2b68654d5739c4d2c69c8b5366a3c3d
Ajaybakshi <46992436+Ajaybakshi2600@users.noreply.github.com>
2026-03-25 16:20:15 +0530

    Add files via upload

---
+++ Telecom Customer Churn Analysis.sql
@@ -0,0 +1,125 @@
+
+Create database db_Churn
+use db_Churn
+select * from Telecom_Customer
+
+---Gender Count-----
+select gender,count(*) as Gender_count,
+cast(count(gender)*1.0*100/(select count(gender) from Telecom_Customer) as decimal(10,2)) as per_of_gender_count
+from Telecom_Customer
+group by gender
+
+
+----Contract Distribution----
+select Contract,count(*) as Contract_count,
+cast(count(Contract)*1.0*100/(select count(contract) from Telecom_Customer) as decimal(10,2)) as per_of_contract_count
+from Telecom_Customer
+group by contract
+
+--Count and revenue contribution of each customer status--
+select customer_status,
+count(customer_id) as total_count,
+sum(total_revenue) as total_revenue,
+cast(sum(total_revenue)*100/ (select sum(Total_Revenue) from Telecom_Customer) as decimal(10,2)) as revenue_percentage
+from Telecom_Customer
+group by customer_status
+
+
+---State Count---
+select state, 
+count(state) as total_count,
+cast(count(state) *100.0 / (select count(*) from Telecom_Customer) as decimal(10,2)) as precentage
+from Telecom_Customer
+group by state
+order by precentage desc
+
+--to check for null values in all columns---
+select 
+    SUM(case when customer_id is null then 1 else 0 end ) as customer_null_count,
+    SUM(CASE WHEN Gender IS NULL THEN 1 ELSE 0 END) AS Gender_Null_Count,
+    SUM(CASE WHEN Age IS NULL THEN 1 ELSE 0 END) AS Age_Null_Count,
+    SUM(CASE WHEN Married IS NULL THEN 1 ELSE 0 END) AS Married_Null_Count,
+    SUM(CASE WHEN State IS NULL THEN 1 ELSE 0 END) AS State_Null_Count,
+    SUM(CASE WHEN Number_of_Referrals IS NULL THEN 1 ELSE 0 END) AS Number_of_Referrals_Null_Count,
+    SUM(CASE WHEN Tenure_in_Months IS NULL THEN 1 ELSE 0 END) AS Tenure_in_Months_Null_Count,
+    SUM(CASE WHEN Value_Deal IS NULL THEN 1 ELSE 0 END) AS Value_Deal_Null_Count,
+    SUM(CASE WHEN Phone_Service IS NULL THEN 1 ELSE 0 END) AS Phone_Service_Null_Count,
+    SUM(CASE WHEN Multiple_Lines IS NULL THEN 1 ELSE 0 END) AS Multiple_Lines_Null_Count,
+    SUM(CASE WHEN Internet_Service IS NULL THEN 1 ELSE 0 END) AS Internet_Service_Null_Count,
+    SUM(CASE WHEN Internet_Type IS NULL THEN 1 ELSE 0 END) AS Internet_Type_Null_Count,
+    SUM(CASE WHEN Online_Security IS NULL THEN 1 ELSE 0 END) AS Online_Security_Null_Count,
+    SUM(CASE WHEN Online_Backup IS NULL THEN 1 ELSE 0 END) AS Online_Backup_Null_Count,
+    SUM(CASE WHEN Device_Protection_Plan IS NULL THEN 1 ELSE 0 END) AS Device_Protection_Plan_Null_Count,
+    SUM(CASE WHEN Premium_Support IS NULL THEN 1 ELSE 0 END) AS Premium_Support_Null_Count,
+    SUM(CASE WHEN Streaming_TV IS NULL THEN 1 ELSE 0 END) AS Streaming_TV_Null_Count,
+    SUM(CASE WHEN Streaming_Movies IS NULL THEN 1 ELSE 0 END) AS Streaming_Movies_Null_Count,
+    SUM(CASE WHEN Streaming_Music IS NULL THEN 1 ELSE 0 END) AS Streaming_Music_Null_Count,
+    SUM(CASE WHEN Unlimited_Data IS NULL THEN 1 ELSE 0 END) AS Unlimited_Data_Null_Count,
+    SUM(CASE WHEN Contract IS NULL THEN 1 ELSE 0 END) AS Contract_Null_Count,
+    SUM(CASE WHEN Paperless_Billing IS NULL THEN 1 ELSE 0 END) AS Paperless_Billing_Null_Count,
+    SUM(CASE WHEN Payment_Method IS NULL THEN 1 ELSE 0 END) AS Payment_Method_Null_Count,
+    SUM(CASE WHEN Monthly_Charge IS NULL THEN 1 ELSE 0 END) AS Monthly_Charge_Null_Count,
+    SUM(CASE WHEN Total_Charges IS NULL THEN 1 ELSE 0 END) AS Total_Charges_Null_Count,
+    SUM(CASE WHEN Total_Refunds IS NULL THEN 1 ELSE 0 END) AS Total_Refunds_Null_Count,
+    SUM(CASE WHEN Total_Extra_Data_Charges IS NULL THEN 1 ELSE 0 END) AS Total_Extra_Data_Charges_Null_Count,
+    SUM(CASE WHEN Total_Long_Distance_Charges IS NULL THEN 1 ELSE 0 END) AS Total_Long_Distance_Charges_Null_Count,
+    SUM(CASE WHEN Total_Revenue IS NULL THEN 1 ELSE 0 END) AS Total_Revenue_Null_Count,
+    SUM(CASE WHEN Customer_Status IS NULL THEN 1 ELSE 0 END) AS Customer_Status_Null_Count,
+    SUM(CASE WHEN Churn_Category IS NULL THEN 1 ELSE 0 END) AS Churn_Category_Null_Count,
+    SUM(CASE WHEN Churn_Reason IS NULL THEN 1 ELSE 0 END) AS Churn_Reason_Null_Count
+FROM Telecom_Customer;
+
+
+--replace null with value
+SELECT 
+    Customer_ID,
+    Gender,
+    Age,
+    Married,
+    State,
+    Number_of_Referrals,
+    Tenure_in_Months,
+    ISNULL(Value_Deal, 'None') AS Value_Deal,
+    Phone_Service,
+    ISNULL(Multiple_Lines, 'No') As Multiple_Lines,
+    Internet_Service,
+    ISNULL(Internet_Type, 'None') AS Internet_Type,
+    ISNULL(Online_Security, 'No') AS Online_Security,
+    ISNULL(Online_Backup, 'No') AS Online_Backup,
+    ISNULL(Device_Protection_Plan, 'No') AS Device_Protection_Plan,
+    ISNULL(Premium_Support, 'No') AS Premium_Support,
+    ISNULL(Streaming_TV, 'No') AS Streaming_TV,
+    ISNULL(Streaming_Movies, 'No') AS Streaming_Movies,
+    ISNULL(Streaming_Music, 'No') AS Streaming_Music,
+    ISNULL(Unlimited_Data, 'No') AS Unlimited_Data,
+    Contract,
+    Paperless_Billing,
+    Payment_Method,
+    Monthly_Charge,
+    Total_Charges,
+    Total_Refunds,
+    Total_Extra_Data_Charges,
+    Total_Long_Distance_Charges,
+    Total_Revenue,
+    Customer_Status,
+    ISNULL(Churn_Category, 'Others') AS Churn_Category,
+    ISNULL(Churn_Reason , 'Others') AS Churn_Reason
+   INTO prod_Churn
+FROM Telecom_Customer;
+
+select * from prod_Churn
+
+---creating a view for power BI:
+create view vw_churndata
+as
+select * from prod_Churn where Customer_Status in ('Stayed','Churned')
+
+create view vw_joineddata
+as
+select * from prod_Churn where Customer_Status in ('joined')
+
+                                                                             
+
+
+
+
